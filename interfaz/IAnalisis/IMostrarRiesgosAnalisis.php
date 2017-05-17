dbc4de865d744f168d613de7478184c41bb1d912
frander170896 <franderramirez@yahoo.es>
2017-05-16 18:22:48 -0600

    Integrando

--- interfaz/IAnalisis/IMostrarRiesgosAnalisis.php
+++ interfaz/IAnalisis/IMostrarRiesgosAnalisis.php
@@ -16,25 +16,19 @@
 	$( document ).ready(function(){
 	$('select').material_select();});
 </script>
-
 <div class="row">
-	<div class="contendorselect">
-		
+	<div class="col l6 m6 s12">
 		<label>Seleccione un departamento</label>
 		<select id="departamentos" name="departamentos" onchange="cargarGUIMostrarRiesgosAnalisis()">
-	
 			<option value="0" disabled="true" selected>Seleccione una opci&oacuten</option>
 			<?php
 				foreach ($listaDepartamentos as $departamento) {
 					echo "<option value=".$departamento->getIdDepartamento().">".$departamento->getNombreDepartamento()."</option>";
 				}
 			?>
-	
 		</select>
-	
-	
 	</div>
 </div>
-
 <div id="mostrarRiesgosAnalisis"></div>
+	
 
