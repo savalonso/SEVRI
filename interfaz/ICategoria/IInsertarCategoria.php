08a764befbbd4eef02a0ab7c252faf5e124bd927
ruben2807 <rubenporrass@gmail.com>
2017-06-10 22:59:25 -0600

    Cambios en validadciones de las categorías
    Ahora las validaciones están en su js respectivo y se valida bien el
    tamaño del nombre de la categoría o subcategoría.

--- interfaz/ICategoria/IInsertarCategoria.php
+++ interfaz/ICategoria/IInsertarCategoria.php
@@ -99,57 +99,7 @@ var statSend = false;
 	   Materialize.updateTextFields();
 	});
 	$(document).ready(function() {
-		if(document.getElementById('categoria').disabled==false){
-			$("#IInsertarCategoria").validate({
-		        rules: {
-		            nombre: { required: true,minlength: 5, maxlength: 100},
-		            descripcion: { required:true,minlength: 20, maxlength: 2000},
-		            tipo: {required : true},
-		        },
-		        messages: {
-		            nombre: "Debe introducir un nombre de categor&iacutea mayor de 5 car&aacutecteres.",
-		            descripcion: "Debe introducir una descripci&oacuten de categor&iacutea mayor de 20 car&aacutecteres.",
-		            tipo: "Debe seleccionar un tipo.",
-		        },
-		        submitHandler: function(form){
-		        	if(document.getElementById('categoria').value==0){
-		        		Materialize.toast("Debe de seleccionar una categor&iacutea v&aacutelida", 7000,'blue darken-3');
-		        	}else{
-
-		        		if (!statSend) {
-                        	statSend = true;
-                        	insertarCategoria();
-                   		 } else {
-                        	Materialize.toast("Ya se esta creando la categoria", 7000,'blue darken-3');
-                    	}
-		        		
-		        	}
-		        }
-		    });
-		}else{
-			$("#IInsertarCategoria").validate({
-		        rules: {
-		            nombre: { required: true,minlength: 5, maxlength: 100},
-		            descripcion: { required:true,minlength: 20, maxlength: 2000},
-		            tipo: {required : true},
-		            categoria: {required: true},
-		        },
-		        messages: {
-		            nombre: "Debe introducir un nombre de categor&iacutea mayor de 5 car&aacutecteres.",
-		            descripcion: "Debe introducir una descripci&oacuten de categor&iacutea mayor de 20 car&aacutecteres.",
-		            tipo: "Debe seleccionar un tipo.",
-		            categoria: "Debe seleccionar una sub categor&iacutea.",
-		        },
-		        submitHandler: function(form){
-		        	if (!statSend) {
-                    	statSend = true;
-                    	insertarCategoria();
-                   	} else {
-                        Materialize.toast("Ya se esta creando la subcategoria", 7000,'blue darken-3');
-                    }
-		        }
-		    });
-		}
+		validarInsertarCategoria();
 	});
 	function verificarCombo(valor){
 		if(valor==0){
