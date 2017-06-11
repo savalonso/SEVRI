aeeaca79ebb339d644c5b728985701a7e86854cf
ruben2807 <rubenporrass@gmail.com>
2017-06-11 15:30:56 -0600

    Correccción de las hps categorías de la mi#%?@

--- interfaz/ICategoria/IMostrarCategoria.php
+++ interfaz/ICategoria/IMostrarCategoria.php
@@ -80,8 +80,12 @@
 					            if($categoria->getHijoDe()==0){
 					            	echo "<tr>					        
 						        	<td>".$categoria->getNombreCategoria()."</td>
-						        	<td>".$categoria->getDescripcion()."</td>
-					        		<td><input class=\"btn btn-default\" type=\"button\" value=\"Modificar\" onclick=\"	cargarPagina('../interfaz/ICategoria/IModificarCategoria.php?idCategoria=".$categoria->getIdCategoria()."')\"/></td>";
+						        	<td>".$categoria->getDescripcion()."</td>";
+						        	if($categoria->getCantRiesgos()>0){
+						        		echo "<td><input class=\"btn btn-default\" disabled=\"true\" type=\"button\" value=\"Modificar\" onclick=\"	cargarPagina('../interfaz/ICategoria/IModificarCategoria.php?idCategoria=".$categoria->getIdCategoria()."')\"/></td>";
+						        	}else{
+					        			echo "<td><input class=\"btn btn-default\" type=\"button\" value=\"Modificar\" onclick=\"	cargarPagina('../interfaz/ICategoria/IModificarCategoria.php?idCategoria=".$categoria->getIdCategoria()."')\"/></td>";
+					        		}
 					        		if ($categoria->getCantHijos()>0 || $categoria->getCantRiesgos()>0){
 					    					echo "<td style=\"text-align:center;\"><button type=\"button\" disabled=\"true\" class=\"btnEliminar\"><a class=\"waves-effect waves-light btn disabled\">Eliminar</a> </button>  </td>
 					    					</tr>";
