70060c9626a09b77bfba205ab263a34bbcb9ce86
savalonso <savalonso@gmail.com>
2017-06-13 00:58:29 -0600

    sulucón conflictos

--- interfaz/IDepartamento/IAgregarUsuarioDepartamento.php
+++ interfaz/IDepartamento/IAgregarUsuarioDepartamento.php
@@ -25,18 +25,20 @@ $listaDepartamento = $controlDepartamento->obtenerDepartamento($idDepartamento);
 $listaUsuarios = $controlUsuarios->obtenerListaUsuarios();
 $listaDepaUsu = $controlDepaUsu->obtenerDepartamentoUsuario($idDepartamento);
 foreach ($listaDepartamento as $departamento) {
-    echo "<h4>Departamento de ".$departamento->getNombreDepartamento()."</h4>";
+    echo "<h4>Agregar usuarios al departamento de ".$departamento->getNombreDepartamento()."</h4>";
 }
 
 ?>
+
+<h4>Lista de usuarios no registrados</h4>
 <div class="row" id="usuarios1">
-    <div class="col s12 m12 l12 blue darken-3 z-depth-5">
-    <h4>Lista de usuarios no registrados</h4>
+    <div class="">
+    
     <?php
     if($listaUsuarios!=null) {
         if($listaDepaUsu != null) {
     ?>
-    <table class="responsive-table centered bordered">
+    <table class="responsive-table striped centered responsive2">
         <thead>
             <tr>
                 <th>Agregar</th>
@@ -67,7 +69,7 @@ foreach ($listaDepartamento as $departamento) {
     <?php
         } else {
     ?>
-    <table class="responsive-table centered bordered">
+    <table class="responsive-table striped centered responsive2">
         <thead>
             <tr>
                 <th>Agregar</th>
@@ -82,27 +84,27 @@ foreach ($listaDepartamento as $departamento) {
         <tbody>
             <?php
             foreach ($listaUsuarios as $usuario){
-                echo "<tr><td><input type=\"button\" value=\"Agregar\" class=\"btn btn-default\" onclick=\"agregarUsuarioDepartamento(".$idDepartamento.",".$usuario->getCedula()."), cargarPagina('../interfaz/IDepartamento/IAgregarUsuarioDepartamento.php?idDepartamento=".$idDepartamento."')\"></td><td>".$usuario->getCedula()."</td><td>".$usuario->getPrimerApellido()."</td><td>".$usuario->getSegundoApellido()."</td><td>".$usuario->getNombre()."</td><td>".$usuario->getCargo()."</td><td>".$usuario->getTipo()."</td></tr>";}
+                echo "<tr><td><input type=\"button\" value=\"Agregar\" class=\"btn btn-default\" onclick=\"agregarUsuarioDepartamento(".$idDepartamento.",'".$usuario->getCedula()."'), cargarPagina('../interfaz/IDepartamento/IAgregarUsuarioDepartamento.php?idDepartamento=".$idDepartamento."')\"></td><td>".$usuario->getCedula()."</td><td>".$usuario->getPrimerApellido()."</td><td>".$usuario->getSegundoApellido()."</td><td>".$usuario->getNombre()."</td><td>".$usuario->getCargo()."</td><td>".$usuario->getTipo()."</td></tr>";}
             ?>
         </tbody>
         </table>
         <?php
         }
     } else{
-        echo "<h4>No hay usuarios registrados</h4>";
+        echo "<h5>No hay usuarios registrados</h5>";
     }
         ?>
     </div>
 </div>
-
+<h4>Lista de usuarios registrados</h4>
 <div class="row" id="usuarios2">
-    <div class="col s12 m12 l12 blue darken-3 z-depth-5">
-    <h4>Lista de usuarios registrados</h4>
+    <div class="">
+    
     <?php  
     if($listaUsuarios!=null) {
         if($listaDepaUsu != null) {
     ?>
-        <table class="responsive-table centered bordered">
+        <table class="responsive-table striped centered responsive2">
             <thead>
                 <tr>
                     <th>Agregar</th>
@@ -119,7 +121,7 @@ foreach ($listaDepartamento as $departamento) {
             foreach ($listaUsuarios as $usuario){
                 foreach($listaDepaUsu as $du){
                     if($du->getCedulaUsuario() == $usuario->getCedula()){
-                        echo "<tr><td><input type=\"button\" value=\"Descartar\" class=\"btn btn-default\" onclick=\"eliminarUsuarioDepartamento(".$idDepartamento.",".$usuario->getCedula()."), cargarPagina('../interfaz/IDepartamento/IAgregarUsuarioDepartamento.php?idDepartamento=".$idDepartamento."')\"></td><td>".$usuario->getCedula()."</td><td>".$usuario->getPrimerApellido()."</td><td>".$usuario->getSegundoApellido()."</td><td>".$usuario->getNombre()."</td><td>".$usuario->getCargo()."</td><td>".$usuario->getTipo()."</td></tr>";
+                        echo "<tr><td><input type=\"button\" value=\"Descartar\" class=\"btn btn-default\" onclick=\"eliminarUsuarioDepartamento(".$idDepartamento.",'".$usuario->getCedula()."'), cargarPagina('../interfaz/IDepartamento/IAgregarUsuarioDepartamento.php?idDepartamento=".$idDepartamento."')\"></td><td>".$usuario->getCedula()."</td><td>".$usuario->getPrimerApellido()."</td><td>".$usuario->getSegundoApellido()."</td><td>".$usuario->getNombre()."</td><td>".$usuario->getCargo()."</td><td>".$usuario->getTipo()."</td></tr>";
                     }
                 }
             }
